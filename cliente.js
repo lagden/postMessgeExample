cbcb2284d26f81fa956d74ed582a76d24c581fb6
Thiago Lagden <lagden@gmail.com>
2015-02-19 21:51:47 -0200

    iframe resize via mutation observer... dev...

--- cliente.js
+++ cliente.js
@@ -1,67 +1,35 @@
+'use strict';
+
 (function(root, factory) {
-
-    'use strict';
-
-    if (typeof define === 'function' && define.amd)
-        define(function() {
-            return (root.iframeResizeClient = factory());
-        });
-    else
-        root.iframeResizeClient = factory();
-
+  if (typeof define === 'function' && define.amd) {
+    define(function() {
+      return (root.IframeResize = factory());
+    });
+  } else {
+    root.IframeResize = factory();
+  }
 }(this, function() {
+  var iframe, iframeOrigin;
 
-    'use strict';
+  function envia(msg) {
+    iframe.contentWindow.postMessage(msg, iframeOrigin);
+  }
 
-    return (function() {
-        var iframe = null,
-            intervalo = null;
+  function recebe(event) {
+    console.debug('cliente', 'recebe', event.data);
+    // iframe.style.height = event.data + 'px';
+  }
 
-        // var iframeOrigin = window.location.protocol + '//lagden.webfactional.com';
-        var iframeOrigin = window.location.protocol + '//10.0.1.56:8080';
+  function init() {
+    window.addEventListener('message', recebe);
+    envia('TExClientInit');
+  }
 
-        function IframeResizeClientException(message) {
-            this.message = message;
-            this.name = "iframeResizeClientException";
-        }
-
-        function envia() {
-            iframe.contentWindow.postMessage('init', iframeOrigin);
-        }
-
-        function recebe(event) {
-            if (event.data === 'initiate')
-                clearInterval(intervalo);
-            else
-                iframe.style.height = event.data + 'px';
-        }
-
-        function addEv(el, eventName, handler, usarCaptura) {
-            usarCaptura = usarCaptura || false;
-            if (el.addEventListener)
-                el.addEventListener(eventName, handler, usarCaptura);
-            else
-                el.attachEvent('on' + eventName, function() {
-                    handler.call(el);
-                });
-        }
-
-        function initiate() {
-            intervalo = setInterval(function() {
-                envia();
-            }, 3000);
-
-            addEv(window, 'message', recebe);
-        }
-
-        return {
-            load: function(id) {
-                iframe = document.querySelector(id);
-                if (iframe !== null)
-                    initiate();
-                else
-                    throw new IframeResizeClientException('iFrame não encontrado');
-            }
-        };
-    })();
+  return {
+    load: function(i, o) {
+      iframe = i;
+      iframeOrigin = o;
+      init();
+    }
+  };
 }));
